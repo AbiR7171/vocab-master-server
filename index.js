53637870af6400d03ea216cde5406147a1507470
AbiR7171 <abiremtiag@gmail.com>
2023-09-18 23:13:20 +0600

    working on chat app appi

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ const express = require("express")
 const cors = require("cors")
 const dotenv = require('dotenv')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const Conversation = require("./models/Conversation");
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 
 
+
+require("./Db/connection");
+
+
+
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 
 app.get("/", (req,res)=>{
     res.send("Vocab Master running on server")
 })
+
+
+// chat app
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -36,12 +58,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
    const wordCollection = client.db("vocab-master").collection("word")
    const lessonCollection = client.db("vocab-master").collection("quizs")
    const usersCollection = client.db("vocab-master").collection("users")
+   const quizCollection = client.db("vocab-master").collection("quiz")
+   const issueCollection = client.db("vocab-master").collection("issue");
+   const feedbackCollection = client.db("vocab-master").collection("feedback");
 
 
 
@@ -62,17 +87,79 @@ async function run() {
    }
 
 
-   app.get("/words", async(req, res)=>{
+  //  app.get("/words", async(req, res)=>{
 
-    const result = await wordCollection.find().toArray()
-    res.send(result)
+  //   const result = await wordCollection.find().toArray()
+  //   res.send(result)
     
+  //  });
+
+
+   app.get("/words", async(req, res)=>{
+     
+          const result = await lessonCollection.find().toArray();
+          res.send(result)
    })
+
 
    app.get("/quiz", async(req, res)=>{
 
-            const result = await lessonCollection.find().toArray();
-            res.send(result)
+    const search = req.query.search;
+
+    const query = {word: { $regex: search, $options: "i"} };
+
+    const result = await lessonCollection.find(query).toArray();
+
+    res.send(result)
+
+   })
+
+
+   app.post("/issue", async(req, res)=>{
+
+        const issue = req.body;
+        console.log(issue);
+
+        const result = await issueCollection.insertOne(issue);
+        res.send(result)
+   })
+
+
+   app.get("/issue", async(req, res)=>{
+
+       const result = await issueCollection.find().toArray();
+       res.send(result)
+   })
+
+
+
+  //  app.get("/quizs", async(req, res)=>{
+       
+     
+  //           const search = req.query.search;
+
+  //           const query = {word: { $regex: search, $options: "i"} };
+
+  //           const result = await lessonCollection.find(query).toArray();
+  //           res.send(result)
+
+
+
+  //  })
+
+   app.delete("/word/:id", async(req, res)=>{
+     const id = req.params.id;
+     const query = {_id : new ObjectId(id)};
+     const result = await lessonCollection.deleteOne(query);
+     res.send(result)
+   })
+
+   app.get("/word/:id" , async(req,res)=>{
+              
+                 const id = req.params.id;
+                 const query = {_id : new ObjectId(id)};
+                 const result = await lessonCollection.find(query).toArray();
+                 res.send(result)
    })
 
 
@@ -118,17 +205,127 @@ async function run() {
        const query = { email : email};
        
         const data = req.body;
-        console.log(data.diamond);
+        console.log(data?.diamond);
         
         const updateDoc = {
                 
             $set:{
-                diamond : data.diamond + 1
+                diamond : data?.diamond + 1
             }
         }
 
         const result = await usersCollection.updateOne(query, updateDoc)
         res.send(result)
+   })
+
+
+   app.patch("/singleUser/users/level", async(req, res)=>{
+     
+              const email = req.query.email;
+              const query = { email : email};
+ 
+              const data = req.body;
+
+              console.log(data.season);
+
+              const updateDoc = {
+                     
+                           $set : {
+                              season : data?.season
+                           }
+              }
+
+              const result = await usersCollection.updateOne(query, updateDoc);
+
+              res.send(result);
+   })
+
+
+
+   app.post("/feedback", async(req, res)=> {
+
+
+              const feedback = req.body;
+              const result = await feedbackCollection.insertOne(feedback);
+              res.send(result)
+   })
+
+
+   app.get("/feedback", async(req, res)=> {
+
+           const result = await feedbackCollection.find().toArray();
+
+           res.send(result)
+   })
+
+
+   app.patch("/feedback/show/:id", async(req, res)=>{
+ 
+               const id = req.params.id;
+               const query = {_id: new ObjectId(id)}
+
+               const updateDoc = {
+                   $set: {
+                        show: true
+                   }
+               }
+           
+               const result = await feedbackCollection.updateOne(query, updateDoc);
+
+               res.send(result)
+   })
+
+
+
+   app.get("/users/student", async(req, res)=>{
+
+
+     const query = {role : "student"};
+     
+     const result = await usersCollection.find(query).sort({diamond: -1}).toArray();
+
+     res.send(result);
+
+
+
+   }); 
+
+
+    app.put("/singleUser/firstSeasonMark/:email", async(req, res)=>{
+
+          const email = req.params.email;
+          const query = {email : email}
+
+          const marks = req.body; 
+          console.log(marks);
+          const options = { upsert: true };
+           const names = Object.keys(marks)[0]       
+ 
+          const updateDoc = {
+              $set : {
+                   quizMarks: [
+                        {
+                           first : marks.first
+                        }
+                   ]
+              }
+          }
+
+
+          const result = await usersCollection.updateOne(query, updateDoc, options);
+          res.send(result)
+
+        
+
+    })
+
+
+
+   app.get("/seasonQuiz", async(req, res)=>{
+
+
+             const result = await quizCollection.find().toArray();
+             res.send(result)
    })
 
 
@@ -178,6 +375,64 @@ async function run() {
    })
 
 
+    
+   app.put("/word/:id", async(req, res)=>{
+     
+              const id = req.params.id;
+              const query = {_id: new ObjectId(id)};
+              const updateData = req.body;
+              const updateDoc ={
+                  $set : {
+                    word : updateData.word,
+                    meaning : updateData.meaning,
+                    category : updateData.category,
+                  }
+              }
+
+              const result = await lessonCollection.updateOne(query, updateDoc);
+              res.send(result)
+   })
+
+
+  //  chat app 
+
+  app.post("/message/conversation", async(req, res)=>{
+
+
+
+    try{
+            const { senderId, receiverId } = req.body;
+            
+                console.log(req.body)
+  
+            const newConversation = new Conversation({ members: [senderId, receiverId]});
+            await newConversation.save();
+            res.status(200).send("Conversation created succesfully");
+    }catch(err){
+       console.log(`erro`, err);
+    }
+  })
+
+
+ 
+
+  app.get("/message/conversation/:userId", async(req, res)=>{
+       
+    try{
+      const userId = req.params.userId;
+      const conversations = await Conversation.find({members: {$in: [userId]}});
+      const conversationUser = Promise.all(conversations.map(async(conversation) => {
+          const receiverId = conversation.members.find(member => member !== userId);
+          const user =  await usersCollection.findOne(new ObjectId(receiverId));
+          return { user: {email: user.email, name: user.name}, conversationId: conversation._id }
+      }))
+      res.status(200).json(await conversationUser);
+    }catch(err){
+         console.log(err, "err");
+    }
+})
+
+
    
 
    
@@ -187,7 +442,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
